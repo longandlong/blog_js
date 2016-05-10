c8150bcd289ad5c7ad48bd86b34437da8bbcc5ec
James long <1181281178@qq.com>
2016-05-11 00:44:27 +0800

    james

--- p.sh
+++ p.sh
@@ -4,4 +4,4 @@ git pull origin master
 git add .
 git commit -a -m "$str"
 git push -u origin master
-ssh root@115.159.217.29 "cd blog/mysite/ && git reset --hard && git pull && python manage.py runserver 0.0.0.0:80"
+ssh root@115.159.217.29 "cd blog/mysite/ && git reset --hard && git pull && python manage.py runserver 0.0.0.0:800"
